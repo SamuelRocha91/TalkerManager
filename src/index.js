c19751b1da5765793942cd24583f7a5576efbc5d
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-04-15 08:30:05 -0300

    requisito 02 - requisicao get paral talkers/is

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -18,6 +19,18 @@ app.listen(PORT, () => {
 
 app.get('/talker', async (req, res) => {
   const data = await findAllJson();
-  if(!data) return  res.status(HTTP_OK_STATUS).json([]);
+  if (!data) return res.status(HTTP_OK_STATUS).json([]);
   res.status(HTTP_OK_STATUS).json(data);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await findAllJson();
+  const talkerId = data.find((element) => element.id === Number(id));
+  if (!talkerId) {
+    return res.status(HTTP_NOT_FOUND).json({
+    message: 'Pessoa palestrante não encontrada',
+  });
+}
+  res.status(HTTP_OK_STATUS).json(talkerId);
 });
